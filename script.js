2bfc8a1a24822ee7539658ea79ebb40a5b9a747d
AjishaKS <ajimon.chindu@gmail.com>
2022-02-03 20:50:46 +0530

    Added Addition functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function add()
 {
-  
-  
+	a = Number(document.getElementById("numA").value);
+  	b = Number(document.getElementById("numB").value);
+  	var c = a + b;
+	document.getElementById("operator").innerHTML = "+";
+	document.getElementById("result").innerHTML = c;
+	document.getElementById("msg").innerHTML = '';
 }
 
 function subtract()
